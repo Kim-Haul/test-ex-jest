0638d7816de7bf59a29261f239afe93243f2ea2b
Kim-Haul <bokugawa@naver.com>
2023-01-18 15:10:51 +0900

    :sparkles: Jest를 이용한 기본 테스트 (비동기 통신)

--- src/test/calculator.js
+++ src/test/calculator.js
@@ -1,5 +1,22 @@
+// ---------------------------------------------- 1번
 const add = (a, b) => {
   return a + b;
 };
 
 export default add;
+
+// ---------------------------------------------- 2번
+export const getNumbers = () => {
+  return fetch('http://localhost:5001/numbers');
+};
+
+// ---------------------------------------------- 3번
+export const addArrayNumbers = (arr) => {
+  let sum = 0;
+
+  arr.map((a) => {
+    sum = sum + a;
+  });
+
+  return sum;
+};
